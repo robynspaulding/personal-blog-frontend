28a4af52c7cf062dc1af202900f242fe1b4fa56a
RobynSpaulding <robynm27@gmail.com>
2022-10-23 14:02:19 -0700

    adds backend post request to login.jsx as well as error handling and jwt token in headers

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,14 +1,49 @@
+import axios from "axios";
+import { useState } from "react";
+
+const jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
+
 export function Login() {
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDafault();
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/sessions", params)
+      .then((response) => {
+        console.log(response.data);
+        axios.defaults.headers.common["Authorization"] = "Bearer" + response.data.jwt;
+        localStorage.setItem("jwt", response.data.jwt);
+        event.target.reset();
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setErrors(["Invalid email or password"]);
+      });
+  };
+
   return (
-    <form id="login">
+    <div id="login">
       <h1>Login</h1>
-      <div>
-        Email: <input name="email" type="email" />
-      </div>
-      <div>
-        Password: <input name="password" type="password" />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Email: <input name="email" type="email" />
+        </div>
+        <div>
+          Password: <input name="password" type="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 }
